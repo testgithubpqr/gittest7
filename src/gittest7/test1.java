c9a5d191174a25e09647dbd627918b084902767a
Admin <Admin@DESKTOP-HEJABK1.localdomain>
2024-02-08 14:08:06 +0530

    tester first commit

--- src/gittest7/test1.java
+++ src/gittest7/test1.java
@@ -5,6 +5,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("master first commit");
+System.out.println("tester first commit");
 	}
 
 }
